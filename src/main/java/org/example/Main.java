6b4be8facf57f75d7ff67e9bfc0fea38b6b27e48
Zack <zacharycollier36@protonmail.com>
2025-03-31 13:48:54 -0230

    Added PostgreSQL JDBC Driver for PostgreSQL connections

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,10 @@
 package org.example;
+/*
+ import org.mindrot.jbcrypt.BCrypt; //for bcrypt
+ import java.sql.Connection;
+ import java.sql.DriverManager;
+ import java.sql.SQLException;
+*/
 
 public class Main {
     public static void main(String[] args) {
