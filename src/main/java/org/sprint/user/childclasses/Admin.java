0d4e6800dbc411a6e6cf8f6554bba1d59733733e
Sarah Murphy <sarahelizabeth.murphy@keyin.com>
2025-04-01 16:17:00 -0230

    Extending Admin, Trainer, Member classes from User

--- src/main/java/org/sprint/user/childclasses/Admin.java
+++ src/main/java/org/sprint/user/childclasses/Admin.java
@@ -1,4 +1,7 @@
 package org.sprint.user.childclasses;
 
-public class Admin {
+public class Admin extends User {
+    public Admin(int userId, String username, String hashedPassword, String email, String phoneNumber, String address) {
+        super(userId, username, hashedPassword, email, phoneNumber, address, "Admin");
+    }
 }
--- src/main/java/org/sprint/user/childclasses/Member.java
+++ src/main/java/org/sprint/user/childclasses/Member.java
@@ -1,4 +1,7 @@
 package org.sprint.user.childclasses;
 
-public class Member {
+public class Member extends User {
+    public Member(int userId, String username, String hashedPassword, String email, String phoneNumber, String address) {
+        super(userId, username, hashedPassword, email, phoneNumber, address, "Member");
+    }
 }
