9b1f89736b54bf4d25297ff49a5df5ef942a2de4
Zack <zacharycollier36@protonmail.com>
2025-04-02 11:11:40 -0230

    Begin extending UserDAO class for SQL queries related to Users

--- src/main/java/org/sprint/memberships/MembershipDAO.java
+++ src/main/java/org/sprint/memberships/MembershipDAO.java
@@ -11,8 +11,10 @@ public class MembershipDAO {
 
     public void addMembership(Membership membership) throws SQLException {
         String sql = "INSERT INTO memberships (member_id, type, description, cost, purchase_date) VALUES (?, ?, ?, ?, ?)";
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement prepstat = conn.prepareStatement(sql))
+        try (
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement prepstat = conn.prepareStatement(sql)
+        )
         {
             prepstat.setInt(1, membership.getUserId());
             // etc etc
--- src/main/java/org/sprint/user/UserDAO.java
+++ src/main/java/org/sprint/user/UserDAO.java
@@ -1,4 +1,28 @@
 package org.sprint.user;
 
+import org.sprint.database.DBConnection;
+
+import java.sql.*;
+
 public class UserDAO {
+    public User getUserByUsername(String username) throws SQLException {
+        String sql = "SELECT * FROM users WHERE username = ?";
+        DriverManager DatabaseConnector;
+        try (
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement prepstat = conn.prepareStatement(sql)
+        )
+        {
+            prepstat.setString(1, username);
+            try (ResultSet resultset = prepstat.executeQuery())
+            {
+                if (resultset.next()){
+                    return new User(
+                        // resultset.getInt("id"),
+                        //etc etc
+                    );
+                }
+            }
+        }
+    }
 }
