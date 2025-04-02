58fe0684852bd70a90d2c04c9064f39a03bb7aee
Zack <zacharycollier36@protonmail.com>
2025-04-02 10:42:09 -0230

    Begin extending MembershipDAO class for SQL queries related to Memberships

--- src/main/java/org/sprint/memberships/MembershipDAO.java
+++ src/main/java/org/sprint/memberships/MembershipDAO.java
@@ -1,4 +1,22 @@
 package org.sprint.memberships;
 
+import org.sprint.database.DBConnection;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MembershipDAO {
+
+    public void addMembership(Membership membership) throws SQLException {
+        String sql = "INSERT INTO memberships (member_id, type, description, cost, purchase_date) VALUES (?, ?, ?, ?, ?)";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement prepstat = conn.prepareStatement(sql))
+        {
+            prepstat.setInt(1, membership.getUserId());
+            // etc etc
+            // prepstat.executeUpdate();
+        }
+    }
 }
