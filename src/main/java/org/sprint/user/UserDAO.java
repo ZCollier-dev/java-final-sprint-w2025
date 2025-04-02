cfb81f683e32287bda3cba741d3756cd955d84a7
Zack <zacharycollier36@protonmail.com>
2025-04-02 14:58:56 -0230

    Finalized UserDAO's CRUD Operations
    - Added addUser INSERT operation which adds a user object to the database
    - Added getAllUsers SELECT operation which gets all users in the database
    - Added updateUser UPDATE operation which updates a user based on a new user object.
    - Added deleteUserByUsername CELETE operation which deletes a user via their username.

--- src/main/java/org/sprint/user/UserDAO.java
+++ src/main/java/org/sprint/user/UserDAO.java
@@ -5,7 +5,51 @@ import org.sprint.database.DBConnection;
 import java.sql.*;
 
 public class UserDAO {
-    public User getUserByUsername(String username) throws SQLException {
+    //NOTE: All passwords should be hashed via BCrypt prior to coming into the DAO.
+    //C
+    public void addUser(User user) throws SQLException { //Insert new user into database - Users should be properly validated prior to entering the DAO.
+        String sql = "INSERT INTO users (username, password, email, phone_no, address, role) VALUES (?, ?, ?, ?, ?, ?)";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement prepstat = conn.prepareStatement(sql)
+        )
+        {
+            prepstat.setString(1, user.getUsername());
+            prepstat.setString(2, user.getHashedPassword());
+            prepstat.setString(3, user.getEmail());
+            prepstat.setString(4, user.getPhoneNumber());
+            prepstat.setString(5, user.getAddress());
+            prepstat.setString(6, user.getRole());
+            prepstat.executeUpdate();
+        }
+    }
+    //R
+    public User getAllUsers() throws SQLException { //Gets all users. Used for Admins.
+        String sql = "SELECT * FROM users";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement prepstat = conn.prepareStatement(sql)
+        )
+        {
+            try (ResultSet resultset = prepstat.executeQuery())
+            {
+                if (resultset.next()){
+                    return new User(
+                            resultset.getInt("id"),
+                            resultset.getString("username"),
+                            resultset.getString("password"),
+                            resultset.getString("email"),
+                            resultset.getString("phone_no"),
+                            resultset.getString("address"),
+                            resultset.getString("role")
+                    );
+                }
+            }
+        }
+        return null;
+    }
+
+    public User getUserByUsername(String username) throws SQLException { //Gets users via username.
         String sql = "SELECT * FROM users WHERE username = ?";
         try (
             Connection conn = DBConnection.getConnection();
@@ -29,5 +73,35 @@ public class UserDAO {
             }
         }
         return null;
+    }
+    //U
+    public void updateUser(User user) throws SQLException { //Updates a user with new data from a User object
+        String sql = "UPDATE users SET username = ?, password = ?, email = ?, phone_no = ?, address = ?, role = ? WHERE id = ?";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement prepstat = conn.prepareStatement(sql)
+        )
+        {
+            prepstat.setString(1, user.getUsername());
+            prepstat.setString(2, user.getHashedPassword());
+            prepstat.setString(3, user.getEmail());
+            prepstat.setString(4, user.getPhoneNumber());
+            prepstat.setString(5, user.getAddress());
+            prepstat.setString(6, user.getRole());
+            prepstat.setInt(7, user.getUserId());
+            prepstat.executeUpdate();
+        }
+    }
+    //D
+    public void deleteUserByUsername(String username) throws SQLException { //Deletes a user from the database via username. Admin only.
+        String sql = "DELETE FROM users WHERE username = ?";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement prepstat = conn.prepareStatement(sql)
+        )
+        {
+            prepstat.setString(1, username);
+            prepstat.executeUpdate();
+        }
     }
 }
