cfa6b0e4094520c5c8df1de0aac2cfcc8c349584
Zack <zacharycollier36@protonmail.com>
2025-04-02 11:39:10 -0230

    Added return null to the end of getUserByUsername and getWorkoutClassByType

--- src/main/java/org/sprint/user/UserDAO.java
+++ src/main/java/org/sprint/user/UserDAO.java
@@ -23,5 +23,6 @@ public class UserDAO {
                 }
             }
         }
+        return null;
     }
 }
--- src/main/java/org/sprint/workoutclasses/WorkoutClassDAO.java
+++ src/main/java/org/sprint/workoutclasses/WorkoutClassDAO.java
@@ -23,5 +23,6 @@ public class WorkoutClassDAO {
                 }
             }
         }
+        return null;
     }
 }
