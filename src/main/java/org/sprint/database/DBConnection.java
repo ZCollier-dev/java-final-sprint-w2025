629b73c192fa6756e87549f46701f85089d3459c
Zack <zacharycollier36@protonmail.com>
2025-04-02 10:13:34 -0230

    Extending DBConnection class to connect the program to a PostgreSQL database

--- src/main/java/org/sprint/database/DBConnection.java
+++ src/main/java/org/sprint/database/DBConnection.java
@@ -1,4 +1,24 @@
 package org.sprint.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/javafinal";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "12345678";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try {
+            DBConnection.getConnection();
+            System.out.println("Connection Successful.");
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
