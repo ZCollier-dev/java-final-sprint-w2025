4e1cc74ec0b5f546722d2d7f8f927017d62a1c38
Zack <zacharycollier36@protonmail.com>
2025-04-03 13:32:23 -0230

    Completed all CRUD operations for WorkoutClassDAO
    - addWorkoutClass CREATE operation added to add a new object to database
    - getWorkoutClassByType and getWorkoutClassByTrainerId READ operations to get workout classes by type and trainer
    - updateWorkoutClass UPDATE operation to update an existing workout class with new info
    - deleteWorkoutClassById DELETE operation to delete a workout class via id

--- src/main/java/org/sprint/workoutclasses/WorkoutClassDAO.java
+++ src/main/java/org/sprint/workoutclasses/WorkoutClassDAO.java
@@ -5,7 +5,24 @@ import org.sprint.database.DBConnection;
 import java.sql.*;
 
 public class WorkoutClassDAO {
-    public WorkoutClass getWorkoutClassByType(String type) throws SQLException {
+    //C
+    public void addWorkoutClass(WorkoutClass workoutClass) throws SQLException { //Insert new workout class into database
+        String sql = "INSERT INTO workoutclasses (trainer_id, type, description, schedule) VALUES (?, ?, ?, ?)";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement prepstat = conn.prepareStatement(sql)
+        )
+        {
+            prepstat.setInt(1, workoutClass.getTrainerId());
+            prepstat.setString(2, workoutClass.getWorkoutClassType());
+            prepstat.setString(3, workoutClass.getWorkoutClassDescription());
+            prepstat.setString(4, workoutClass.getSchedule());
+            prepstat.executeUpdate();
+        }
+    }
+
+    //R
+    public WorkoutClass getWorkoutClassByType(String type) throws SQLException { //Get all workout classes of a specified type.
         String sql = "SELECT * FROM workoutclasses WHERE type = ?";
         try (
                 Connection conn = DBConnection.getConnection();
@@ -17,12 +34,68 @@ public class WorkoutClassDAO {
             {
                 if (resultset.next()){
                     return new WorkoutClass(
-                            // resultset.getInt("id"),
-                            //etc etc
+                            resultset.getInt("id"),
+                            resultset.getString("type"),
+                            resultset.getString("description"),
+                            resultset.getInt("trainer_id"),
+                            resultset.getString("schedule")
                     );
                 }
             }
         }
         return null;
+    }
+
+    public WorkoutClass getWorkoutClassByTrainerId(int id) throws SQLException { //Get all workout classes from a specified trainer's id.
+        String sql = "SELECT * FROM workoutclasses WHERE trainer_id = ?";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement prepstat = conn.prepareStatement(sql)
+        )
+        {
+            prepstat.setInt(1, id);
+            try (ResultSet resultset = prepstat.executeQuery())
+            {
+                if (resultset.next()){
+                    return new WorkoutClass(
+                            resultset.getInt("id"),
+                            resultset.getString("type"),
+                            resultset.getString("description"),
+                            resultset.getInt("trainer_id"),
+                            resultset.getString("schedule")
+                    );
+                }
+            }
+        }
+        return null;
+    }
+    //U
+    public void updateWorkoutClass(WorkoutClass workoutClass) throws SQLException { //Update workout class with new data
+        String sql = "UPDATE workoutclasses SET trainer_id = ?, type = ?, description = ?, schedule = ? WHERE id = ?";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement prepstat = conn.prepareStatement(sql)
+        )
+        {
+            prepstat.setInt(1, workoutClass.getTrainerId());
+            prepstat.setString(2, workoutClass.getWorkoutClassType());
+            prepstat.setString(3, workoutClass.getWorkoutClassDescription());
+            prepstat.setString(4, workoutClass.getSchedule());
+            prepstat.setInt(5, workoutClass.getWorkoutClassId());
+            prepstat.executeUpdate();
+        }
+    }
+
+    //D
+    public void deleteWorkoutClassById(int id) throws SQLException { //Deletes a workout class from the database via id.
+        String sql = "DELETE FROM workoutclasses WHERE id = ?";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement prepstat = conn.prepareStatement(sql)
+        )
+        {
+            prepstat.setInt(1, id);
+            prepstat.executeUpdate();
+        }
     }
 }
